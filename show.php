8244eccfcfe5b3fd69e8dd675afc6211799b3c5f
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-27 09:55:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,22 +10,22 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM ExameLAB');
 ?>
 <table width="600" border="1">
   <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="100"> <div align="center">Product </div></th>
+    <th width="350"> <div align="center">Price </div></th>
+    <th width="150"> <div align="center">Amount </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['Product'];?></div></td>
+    <td><?php echo $Result['Price'];?></td>
+    <td><?php echo $Result['Amount'];?></td>
   </tr>
 <?php
 }
