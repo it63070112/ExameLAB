51fb2a96653970d6d77b9e87298ad279c0e45068
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-27 12:46:44 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,7 +1,7 @@
 <html lang="en">
 
 <head>
-    <title>ExameLAB</title>
+    <title>Add values</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
