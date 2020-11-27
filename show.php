8bacdd2efa99c0bf96dd17ebf8010755d676d28b
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-27 12:40:52 +0700

    Update show.php

--- show.php
+++ show.php
@@ -13,7 +13,7 @@
 <body>
     <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'pornpaisan112.mysql.database.azure.com', 'pps112@pornpaisan112', 'Pornpaisan112', 'itflab', 3306);
+mysqli_real_connect($conn, 'pornpaisan112.mysql.database.azure.com', 'pps112@pornpaisan112', 'Pornpaisan112', 'examelab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
